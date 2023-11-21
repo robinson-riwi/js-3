de590661e856c6086c2b86a095a48c6b6ecac025
Robinson Andres Cortes RIWI <147889972+robinson-riwi@users.noreply.github.com>
2023-11-21 15:51:47 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+function operaciones(){
+   
+    let operaciones = document.getElementById('operacion').value;
+
+    if(operaciones == 'suma'){
+        console.log("deberias hacer una suma");
+        // numero1 + numero2
+    }
+   
+
+}
